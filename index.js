3b837781655c981b02cfa82d1fb359dfc1c23cab
AbhikJadav <48055286+AbhikJadav@users.noreply.github.com>
2022-01-12 01:22:27 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,20 +1,59 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import Incdec from './Incdec';
-// import AddIcon from '@mui/icons-material/Add';
-// import reportWebVitals from './reportWebVitals';
+var data = [];
+// localStorage.clear();
+// addtotable();
+const add = () => {
+    document.getElementById("errnm").innerHTML = "";
+    var nm = document.getElementById("name").value;
+    var age = document.getElementById("age").value;
 
-ReactDOM.render(
+    data.push(
+        {
+            id: data.length + 1,
+            name: nm,
+            age: age,
+        }
+    )
 
-  // <App/>,
-  <Incdec/>,
+    const setdata = JSON.stringify(data);
+    localStorage.setItem("userdata", setdata);
 
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-
+    document.getElementById("name").value = " ";
+    document.getElementById("age").value = " ";
+    addtotable();
+    // console.log(setdata);
+    // console.log(data);
+}
+const addtotable = () => {
+    const getdata = JSON.parse(localStorage.getItem("userdata"));
+    // console.log(JSON.parse(getdata));
+    // console.log(getdata);
+    // if(getdata!=null)
+    // {
+        var table = " ";
+        getdata.forEach((value, index) => {
+            var id = index + 1;
+            table += `<tr>
+                <td>${id}</td>
+                <td>${value.name}</td>
+                <td>${value.age}</td>
+                <td><button>Edit</button><button onclick="del(${id})">Delete</button></td>
+            </tr>`
+        });
+        // console.log(table);
+        document.getElementById("root").innerHTML = table;
+    // }
+    // else
+    // {
+    //     document.getElementById("root").innerHTML = "No Data Found";
+    // }
+}
+const del=(id)=>{
+    const getdata = JSON.parse(localStorage.getItem("userdata"));
+    // console.log(getdata);
+    getdata.splice(id,1);
+    console.log(getdata);
+    const setdata = JSON.stringify(data);
+    localStorage.setItem("userdata", setdata);
+    
+    addtotable();
+}
